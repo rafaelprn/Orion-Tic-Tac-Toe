7999369f7e3189258e46ddd5d1b42aaa6143eca8
Rafael Perina <rafael.prn3@gmail.com>
2022-10-01 17:58:13 -0300

    feat: added p5

---
+++ Orion Tic Tac Toe/p5/sketch.js
@@ -0,0 +1,12 @@
+function setup() {
+    createCanvas(windowWidth/2, windowHeight/2);
+  }
+  
+  function draw() {
+    background(0);
+    stroke(255)
+    line(300, 100)
+    line(100, 300, 300, 300)
+    
+  }
+  
