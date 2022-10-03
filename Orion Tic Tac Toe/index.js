55809f0ebe66b26669c6bb11d71d3e303e4fd34f
Rafael Perina <rafael.prn3@gmail.com>
2022-10-03 10:18:48 -0300

    feat: gradient on css

--- Orion Tic Tac Toe/index.js
+++ Orion Tic Tac Toe/index.js
@@ -1,8 +1,16 @@
+const cellElements = document.querySelectorAll("[data-cell]")
+
+//cada jogador tem um vetor com as posicoes preenchidas
+//checo o vetor do player com o vetor winPositions para comparar os arrays
+
 //posições vencedoras 
-let winPos = [
-    [1, 2, 3],
-    [1, 5, 9],
+let winPositions = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [0, 4, 8],
+    [2, 4, 6],
+    [0, 3, 6],
+    [2, 5, 8],
     [1, 4, 7],
-    [3, 6, 9],
-    [7, 8, 9]
+    [6, 7, 8]
 ]
