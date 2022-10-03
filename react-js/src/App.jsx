1f11b8a76f90de23c23cf244815c07e3484e0106
Rafael Perina <rafael.prn3@gmail.com>
2022-10-03 10:37:44 -0300

    feat: added react-js on master

---
+++ react-js/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import {Table} from './components/Table'
+
+function App() {
+
+  return (
+    <>
+    <Table text='Jogo da Velha'/>
+    
+    </>
+  )
+}
+
+export default App
