13dd087998b9056341329c1a4757de306ccc8340
Rafael Perina <rafael.prn3@gmail.com>
2022-10-08 00:14:38 -0300

    comments js

--- src/index.js
+++ src/index.js
@@ -4,8 +4,8 @@ const player2 = "O";
 let text = document.getElementsByClassName("game-status");
 //rounds: X = true / O = false
 let checkRound = true;
-let p1Positions = [];
-let p2Positions = [];
+let p1Positions = []; //array de posicoes do X
+let p2Positions = []; //array de posicoes do O
 //posições vencedoras
 const winPositions = [
   [0, 1, 2],
@@ -18,11 +18,8 @@ const winPositions = [
   [6, 7, 8],
 ]; 
 
-//cada jogador tem um vetor com as posicoes preenchidas
-//checo o vetor do player com o vetor winPositions para comparar os arrays
 document.addEventListener("click", (event) => {
-  if (event.target.matches(".cell")) {
-    //se o jogador clicou no tabuleiro
+  if (event.target.matches(".cell")) { //se o jogador clicou no tabuleiro
     play(event.target.id); //chama a funcao play com o id da celula clicada
   }
 }); 
@@ -42,16 +39,16 @@ function play(id) {
     }
     if (checkRound) {
       //se for true, se for vez do X
-      p1Positions.push(parseInt(cell.id));
-    } else {
-      p2Positions.push(parseInt(cell.id));
+      p1Positions.push(parseInt(cell.id)); //coloca a posicao clicada no vetor
+    } else { //se for a vez do O
+      p2Positions.push(parseInt(cell.id)); 
     }
-    isWinner();
+    isWinner(); //verifica se na ultima jogada houve algum vencedor
 
-    const isDraw = checkDraw();
-    if(isDraw){ //se deu empate
+    const isDraw = checkDraw(); //verifica se houve algum empate
+    if(isDraw){ //se empatou
       text[0].textContent = "Deu Velha!"
-      lockGrid();
+      lockGrid(); //fim do jogo
     }
     checkRound = !checkRound; //inverte o estado o checkRound pra passar o player
   }
@@ -60,13 +57,13 @@ function play(id) {
 function isWinner() { //verifica o vencedor
   const winner = winPositions.some((combination) => {
     return combination.every((index) => {
-      return cellElements[index].classList.contains(checkRound ? "X" : "O");
+      return cellElements[index].classList.contains(checkRound ? "X" : "O"); 
     });
   });
-  if (winner && checkRound) {
+  if (winner && checkRound) {  //se alguem venceu e o X que jogou por ultimo
     text[0].textContent = "X venceu!";
     lockGrid();
-  } else if (winner && !checkRound) {
+  } else if (winner && !checkRound) { //se alguem venceu e o O que jogou por ultimo
     lockGrid();
     text[0].textContent = "O venceu!";
   }
@@ -82,19 +79,19 @@ function unlockgrid() {
 }
 
 const checkDraw = () => {
-  return [...cellElements].every((cell) => {
-    return cell.classList.contains("X") || cell.classList.contains("O")
+  return [...cellElements].every((cell) => { //todos as celulas
+    return cell.classList.contains("X") || cell.classList.contains("O") //se em todas as celulas tem ou X ou O marcados
   })
 }
 
-function restart() {
+function restart() { //botao restart
   const table = document.getElementsByClassName("cell");
   for (let i = 0; i < table.length; i++) {
     table[i].textContent = ""; //limpa tudo
     table[i].classList.remove("X");
     table[i].classList.remove("O");
   }
-  checkRound = true; //reiniciar com o X
+  checkRound = true; //reiniciar com o X como player 1
   text[0].textContent = "Vez de: X";
   unlockgrid();
 }
